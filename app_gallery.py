1fab17f4e99fe4a1c6c412313b6ffc3500594078
Group <128438027+ProjectGroupInfo@users.noreply.github.com>
2023-03-23 10:08:02 +0700

    Add files via upload

--- app_gallery.py
+++ app_gallery.py
@@ -9,11 +9,11 @@ st.write("Here are some of the apps:")
 col1, col2, col3, col4, col5 = st.columns(5)
 
 # URLs of the images in the GitHub repository
-img1_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img1.jpg?raw=true"
-img2_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img2.jpg?raw=true"
-img3_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img3.jpg?raw=true"
-img4_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img4.jpg?raw=true"
-img5_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img5.jpg?raw=true"
+img1_url = "https://github.com/hennypurwadi/app_gallery1/blob/main/img/img1.jpg?raw=true"
+img2_url = "https://github.com/hennypurwadi/app_gallery1/blob/main/img/img2.jpg?raw=true"
+img3_url = "https://github.com/hennypurwadi/app_gallery1/blob/main/img/img3.jpg?raw=true"
+img4_url = "https://github.com/hennypurwadi/app_gallery1/blob/main/img/img4.jpg?raw=true"
+img5_url = "https://github.com/hennypurwadi/app_gallery1/blob/main/img/img5.jpg?raw=true"
 
 # Display the images and create clickable links
 with col1:
@@ -23,10 +23,10 @@ with col1:
     st.markdown('[View source code →](https://github.com/hennypurwadi/pdf_to_img_converter)')
     
 with col2:
-    st.markdown(f'[![Image to Text converter]({img2_url})](https://hennypurwadi-collection-apps-img2text-m5cjli.streamlit.app/)')
+    st.markdown(f'[![Image to Text converter]({img2_url})](https://projectgroupinfo-apps-gallery-image-to-textimg2text-bmrx0m.streamlit.app/)')
     st.write("Image to text converter")
-    st.markdown('[Go to App →](https://hennypurwadi-collection-apps-img2text-m5cjli.streamlit.app/)')
-    st.markdown('[View source code →](https://github.com/hennypurwadi/Image_to_Text_Converter)')
+    st.markdown('[Go to App →](https://projectgroupinfo-apps-gallery-image-to-textimg2text-bmrx0m.streamlit.app/)')
+    st.markdown('[View source code →](https://github.com/ProjectGroupInfo/apps_gallery/tree/main/image_to_text)')
 
 with col3:    
     st.markdown(f'[![PDF Summarizer]({img3_url})](https://hennypurwadi-pdf-summary-pdf-summary-xlstst.streamlit.app/)')
@@ -44,31 +44,4 @@ with col5:
     st.markdown(f'[![PDF Combiners]({img5_url})](https://projectgroupinfo-apps-gallery-combine-pdfscombine-pdf-xtq139.streamlit.app/)')
     st.write("PDF combiners")
     st.markdown('[Go to App →](https://projectgroupinfo-apps-gallery-combine-pdfscombine-pdf-xtq139.streamlit.app/)')
-    st.markdown('[View source code →](https://github.com/ProjectGroupInfo/apps_gallery/tree/main/combine_pdfs)')
-    
-# #FOR FUTURE APPS    
-    
-# col6, col7, col8, col9, col10 = st.columns(5)
-
-# # URLs of the images in the GitHub repository
-# img6_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img6.jpg?raw=true"
-# img7_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img7.jpg?raw=true"
-# img8_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img8.jpg?raw=true"
-# img9_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img9.jpg?raw=true"
-# img10_url = "https://github.com/hennypurwadi/app_gallery/blob/main/img/img10.jpg?raw=true"
-
-# # Display the images and create clickable links
-# with col6:
-#     st.write("Speech to text converter")
-    
-# with col7:
-#     st.write("Extract Data from PDF")
-
-# with col8:    
-#     st.write("Settlement Predictor App")
-    
-# with col9:    
-#     st.write("Future App")
-
-# with col10:
-#     st.write("Other App")
+    st.markdown('[View source code →](https://github.com/hennypurwadi/combine_pdfs)')
